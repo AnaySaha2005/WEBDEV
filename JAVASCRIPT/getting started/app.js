3dac6b4a4d86c0b4181e92aeec0b4965921abfdf
Anay Saha <anaysaha2005@gmail.com>
2024-01-16 22:14:51 +0530

    .

--- JAVASCRIPT/getting started/app.js
+++ JAVASCRIPT/getting started/app.js
@@ -1,31 +1,37 @@
-console.log('Hello World!');
-let i=4;
-if(i!=0){
-    console.log(i);
-}
-else{
-    console.log(0);
-}
-// for(i=3;i>=0;i--)console.log(i);
-let b=true;
-console.log(typeof(b));
+// console.log('Hello World!');
+// let i=4;
+// if(i!=0){
+//     console.log(i);
+// }
+// else{
+//     console.log(0);
+// }
+// // for(i=3;i>=0;i--)console.log(i);
+// let b=true;
+// console.log(typeof(b));
 // alert("something is right...");
 // console.warn("this is msg");
 // let roll=prompt("Enter your roll");
 // if(roll==1234)
 // console.log("Welcome anay");
-let arr=[];
-arr[0]=1;
-arr[1]=2;
-arr.pop();
-arr.push(2);
-arr.unshift(9);
-console.log(arr);
-console.log(arr.indexOf(1));
-arr.sort();
-console.log(arr);
-console.log('length is',arr.length);
-const a=[1,2,3,4];
-a=[2,3,5];
-console.log(a);
+// let arr=[];
+// arr[0]=1;
+// arr[1]=2;
+// arr.pop();
+// arr.push(2);
+// arr.unshift(9);
+// console.log(arr);
+// console.log(arr.indexOf(1));
+// arr.sort();
+// console.log(arr);
+// console.log('length is',arr.length);
+// const a=[1,2,3,4];
+// a=[2,3,5];
+// console.log(a);
+let obj={
+    name:"Anay",
+    age:18,
+    year: "1st"
+};
+console.log(obj);
 
