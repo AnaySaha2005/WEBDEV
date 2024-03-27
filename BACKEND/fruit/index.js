fead9b09753e1198d9fe6fa19e6e0b7e798dcffe
Anay Saha <anaysaha2005@gmail.com>
2024-03-27 17:38:40 +0530

    .

---
+++ BACKEND/fruit/index.js
@@ -0,0 +1,5 @@
+module.exports={
+    v1:require("./apple"),
+    v2:require("./mango"),
+    v3:require("./grapes")
+}
---
+++ BACKEND/script.js
@@ -0,0 +1,4 @@
+const random=require("./fruit")
+console.log(random.v1)
+console.log(random.v2)
+console.log(random.v3)
