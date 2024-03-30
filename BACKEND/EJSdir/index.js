06d44b4a881330466bff05bb183a747db8059d29
Anay Saha <anaysaha2005@gmail.com>
2024-03-30 17:17:33 +0530

    .

--- BACKEND/EJSdir/index.js
+++ BACKEND/EJSdir/index.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const app=express();
+const path=require("path");
+app.set("views",path.join(__dirname,"views"))
 const port=8080;
 app.listen(port,()=>{
     console.log("app is listening")
 })
-app.use((req,res)=>{
-    res.render("home.ejs")
+// app.use((req,res)=>{
+//     let diceval= Math.floor(Math.random()*6)+1 ;
+//     res.render("rolldice.ejs",{num:diceval})
+// })
+app.get("/ig/:username",(req,res)=>{
+let {username}=req.params;
+res.render("ig.ejs",{username})
 })
