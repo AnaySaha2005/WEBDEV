8c4f7be9f991f2c6a893b34fdea8558aa34f7485
Anay Saha <anaysaha2005@gmail.com>
2024-04-04 13:08:03 +0530

    .

--- BACKEND/RESTAPI/index.js
+++ BACKEND/RESTAPI/index.js
@@ -8,18 +8,23 @@ app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.listen(port, () => console.log(`app listening on port ${port}!`))
+const { v4: uuidv4 } = require('uuid');
+uuidv4();// auto id generated
 let posts=[
     {
         username:"anay saha",
-        content:"i love coding!"
+        content:"i love coding!",
+        id:uuidv4()
     },
     {
         username:"Ruma saha",
-        content:"i love Beating!"
+        content:"i love Beating!",
+       id:uuidv4()
     },
     {
         username:"Shrija saha",
-        content:"i love Sleeping!"
+        content:"i love Sleeping!",
+        id:uuidv4()
     }
 ]
 app.get('/posts', (req, res) => {
@@ -30,7 +35,19 @@ app.get('/posts/new',(req,res)=>{
 
 })
 app.post('/posts',(req,res)=>{
-   let {username,content}=req.body
-   posts.push({username,content})
+   let {username,content,id}=req.body
+   posts.push({username,content,id:uuidv4()})
     res.redirect("http://localhost:8080/posts")
+})
+app.get('/posts/:id',(req,res)=>{
+    let {id}=req.params
+    let post=posts.find((p)=>id===p.id)
+    if(post)
+    res.render("show.ejs",{post})
+else res.send("invalid")
+})
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>id===p.id)
+    res.render("edit.ejs",{post})
 })
--- BACKEND/RESTAPI/public/app.js
+++
@@ -1,4 +0,0 @@
-let btn=document.querySelector("#new")
-btn.onclick=()=>{
-    location.href="http://localhost:8080/posts/new"
-}
