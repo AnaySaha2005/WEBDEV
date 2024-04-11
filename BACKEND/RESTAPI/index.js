c4a47a1adfa312eb8d9f78f5568aa1c38e2c0bd4
Anay Saha <anaysaha2005@gmail.com>
2024-04-11 18:06:43 +0530

    .

--- BACKEND/RESTAPI/index.js
+++ BACKEND/RESTAPI/index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const port = 8080
 const path=require("path")
+const { v4: uuidv4 } = require('uuid');
+const methodOverride=require("method-override")
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"./views"))
 app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.listen(port, () => console.log(`app listening on port ${port}!`))
-const { v4: uuidv4 } = require('uuid');
 uuidv4();// auto id generated
+app.use(methodOverride('_method'))
 let posts=[
     {
         username:"anay saha",
@@ -50,4 +52,17 @@ app.get("/posts/:id/edit",(req,res)=>{
     let {id}=req.params;
     let post=posts.find((p)=>id===p.id)
     res.render("edit.ejs",{post})
+})
+app.patch("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let content=req.body.content;
+    let post=posts.find((p)=>id===p.id)
+    post.content=content;
+    console.log(content)
+    res.redirect("http://localhost:8080/posts")
+})
+app.delete("/posts/:id",(req,res)=>{
+  let{id}=req.params;
+   posts=posts.filter((p)=>p.id!==id)
+   res.redirect("/posts")
 })
